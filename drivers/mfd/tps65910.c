0621f9f3a2ffd4aa705ad3de859c696a3ccb6b5c
Dmitry Osipenko <digetx@gmail.com>
2020-05-16 10:34:38 +0300

    mfd: tps65910: Try a unified reg-write for power-off
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/mfd/tps65910.c
+++ drivers/mfd/tps65910.c
@@ -444,12 +444,9 @@ static void tps65910_power_off(void)
 			DEVCTRL_DEV_SLP_MASK) < 0)
 		return;
 
-	if (tps65910_reg_clear_bits(tps65910, TPS65910_DEVCTRL,
-			DEVCTRL_DEV_ON_MASK) < 0)
-		return;
-
-	tps65910_reg_set_bits(tps65910, TPS65910_DEVCTRL,
-			DEVCTRL_DEV_OFF_MASK);
+	tps65910_reg_update_bits(tps65910, TPS65910_DEVCTRL,
+				 DEVCTRL_DEV_OFF_MASK | DEVCTRL_DEV_ON_MASK,
+				 DEVCTRL_DEV_OFF_MASK);
 }
 
 static int tps65910_i2c_probe(struct i2c_client *i2c,
